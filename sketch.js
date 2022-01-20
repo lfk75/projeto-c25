2260344738a56eb9b72e815bf677ce2a922c5cfa
lfk75 <92749457+lfk75@users.noreply.github.com>
2022-01-20 09:27:26 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -67,19 +67,6 @@ function draw() {
 }
 
 function keyPressed() {
-  // if (keyCode === 23) {
-  //   if (numberOfArrows > 0) {
-  //     var posX = playerArcher.body.position.x;
-  //     var posY = playerArcher.body.position.y;
-  //     var angle = playerArcher.body.angle;
-
-  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-  //     Matter.Body.setAngle(arrow.body, angle);
-  //     playerArrows.push(arrow);
-  //     numberOfArrows -= 1;
-  //   }
-  // }
 
     if (keyCode === 32) {
      if (numberOfArrows > 0) {
@@ -95,33 +82,7 @@ function keyPressed() {
      }
    }
 
-   // if (keyCode === 32) {
-  //   if (numberOfArrows > 0) {
-  //     var posX = playerArcher.body.position.x;
-  //     var posY = playerArcher.body.position.y;
-  //     var angle = playerArcher.body.angle;
-
-  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-  //     Matter.Body.setAngle(arrow.body, angle);
-  //     playerArrows.push(arrow);
-  //     numberOfArrows -= 1;
-  //   }
-  // }
-
-   // if (keyCode === 32) {
-  //   if (numberOfArrows > 0) {
-  //     var posX = playerArcher.body.position.x;
-  //     var posY = playerArcher.body.position.y;
-  //     var angle = playerArcher.body.angle;
-
-  //     var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
-
-  //     Matter.Body.setAngle(arrow.body, angle);
-  //     playerArrows.push(arrow);
-  //     numberOfArrows *= 1;
-  //   }
-  // }
+ 
 }
 
 function keyReleased() {
